4e27d45747c8c88e49a1c5e3c20897c6fe834883
UberDog711 <jamesrubyportland@gmail.com>
2026-05-26 04:33:01 -0700

    filters

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 // Draws red circles when you click the mouse
 // Uses a BufferedImage so the graphics don't disappear
@@ -16,6 +18,9 @@ class Main {
     BufferedImage myImage;
     LoadButton loadButton;
     SaveButton saveButton;
+    InvertButton invertButton;
+    BlurButton blurButton;
+
     private int solvedHeight;
     private int solvedWidth;
 
@@ -23,24 +28,25 @@ class Main {
         myImage = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
         loadButton = new LoadButton("Load Image");
         saveButton = new SaveButton("Save Image");
+        invertButton = new InvertButton("Invert Image");
+        blurButton = new BlurButton("Blur Image");
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));    // try Y_AXIS
-        
+
 
         myGraphicsPanel = new GraphicsExamplePanel(800, 600);
 
         buttonPanel.add(loadButton);
         buttonPanel.add(saveButton);
+        buttonPanel.add(invertButton);
+        buttonPanel.add(blurButton);
 
-        mainPanel.add(buttonPanel,BorderLayout.WEST);
+        mainPanel.add(buttonPanel, BorderLayout.WEST);
         mainPanel.add(myGraphicsPanel, BorderLayout.CENTER);
-
-
-
 
 
         JFrame myJFrame = new JFrame("Graphics Example 2");
@@ -53,7 +59,7 @@ class Main {
         myJFrame.setVisible(true);
     }
 
-    ///////////////////////////////////////////////////////////////////
+    /// ////////////////////////////////////////////////////////////////
 
     class GraphicsExamplePanel extends JPanel {
 
@@ -80,9 +86,9 @@ class Main {
             solvedWidth = panelWidth;
 
             if (panelRatio > imageRatio) {
-                    solvedWidth =  (int)(imageRatio * solvedHeight);
+                solvedWidth = (int) (imageRatio * solvedHeight);
             } else if (panelRatio < imageRatio) {
-                    solvedHeight = (int)(solvedWidth / imageRatio);
+                solvedHeight = (int) (solvedWidth / imageRatio);
             }
 
             pen.drawImage(myImage, 0, 0, solvedWidth, solvedHeight, null);
@@ -90,12 +96,12 @@ class Main {
 
     }
 
-    ///////////////////////////////////////////////////////////////////
+    /// ////////////////////////////////////////////////////////////////
 
     class MyMouseListener implements MouseListener, MouseMotionListener {
         private int lastPressedX;
         private int lastPressedY;
-        private Tool currentTool = Tool.POINT``;
+        private Tool currentTool = Tool.POINT;
 
         enum Tool {
             SCRIBBLE,
@@ -107,6 +113,7 @@ class Main {
         public void setTool(Tool tool) {
             currentTool = tool;
         }
+
         @Override
         public void mousePressed(MouseEvent e) {
             double transform = (double) myImage.getWidth() / solvedWidth;
@@ -114,15 +121,14 @@ class Main {
             int y = (int) (e.getY() * transform);
 
 
-
-
             Graphics pen = myImage.getGraphics();
             pen.setColor(Color.RED);
             int brushSize = 10;
             if (currentTool == Tool.POINT) {
-                pen.fillOval((x-brushSize/2), (y-brushSize/2), brushSize, brushSize);
-            } if (currentTool == Tool.STRAIGHTLINE) {
-                pen.drawLine(x,y,lastPressedX,lastPressedY);
+                pen.fillOval((x - brushSize / 2), (y - brushSize / 2), (int) (brushSize * transform), (int) (brushSize * transform));
+            }
+            if (currentTool == Tool.STRAIGHTLINE) {
+                pen.drawLine(x, y, lastPressedX, lastPressedY);
             }
 
             myGraphicsPanel.repaint();
@@ -144,7 +150,7 @@ class Main {
             Graphics pen = myImage.getGraphics();
             pen.setColor(Color.RED);
             if (currentTool == Tool.SCRIBBLE) {
-                pen.drawLine(x,y,lastPressedX,lastPressedY);
+                pen.drawLine(x, y, lastPressedX, lastPressedY);
             }
             myGraphicsPanel.repaint();
             lastPressedX = x;
@@ -168,10 +174,9 @@ class Main {
         }
     }
 
-    ///////////////////////////////////////////////////////////////////
+    /// ////////////////////////////////////////////////////////////////
 
-    class LoadButton extends JButton implements ActionListener
-    {
+    class LoadButton extends JButton implements ActionListener {
         LoadButton(String name) {
             super(name);             // calls the super class (JButton) constructor
             addActionListener(this);       // adds this object (itself) as its own action listener
@@ -180,7 +185,7 @@ class Main {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.showSaveDialog(myGraphicsPanel);
-            if (JFileChooser.APPROVE_OPTION ==0) {
+            if (JFileChooser.APPROVE_OPTION == 0) {
                 try {
                     myImage = ImageIO.read(fileChooser.getSelectedFile());
                     myGraphicsPanel.repaint();
@@ -192,10 +197,9 @@ class Main {
         }
     }
 
-    //////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////
 
-    class SaveButton extends JButton implements ActionListener
-    {
+    class SaveButton extends JButton implements ActionListener {
         SaveButton(String name) {
             super(name);             // calls the super class (JButton) constructor
             addActionListener(this);       // adds this object (itself) as its own action listener
@@ -204,9 +208,9 @@ class Main {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.showSaveDialog(myGraphicsPanel);
-            if (JFileChooser.APPROVE_OPTION ==0) {
+            if (JFileChooser.APPROVE_OPTION == 0) {
                 try {
-                    ImageIO.write(myImage,"png",fileChooser.getSelectedFile());
+                    ImageIO.write(myImage, "png", fileChooser.getSelectedFile());
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -214,18 +218,88 @@ class Main {
         }
     }
 
-    //////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////
 
-    class MySlider extends JSlider implements ChangeListener
-    {
-        MySlider() {
-            super();
-            addChangeListener(this);
+    class InvertButton extends JButton implements ActionListener {
+        InvertButton(String name) {
+            super(name);             // calls the super class (JButton) constructor
+            addActionListener(this);       // adds this object (itself) as its own action listener
         }
 
-        @Override
-        public void stateChanged(ChangeEvent e) {
-            ;
+        public void actionPerformed(ActionEvent e) {
+
+            for (int x = 0; x < myImage.getWidth(); x++) {
+                for (int y = 0; y < myImage.getHeight(); y++) {
+                    Color pixelColor = new Color(myImage.getRGB(x, y));
+                    Color newColor = new Color(
+                            255 - pixelColor.getRed(),
+                            255 - pixelColor.getGreen(),
+                            255 - pixelColor.getBlue());
+                    myImage.setRGB(x, y, newColor.getRGB());
+                }
+
+            }
+            myGraphicsPanel.repaint();
+        }
+    }
+
+    /// ///////////////////////////////////////////////////////////////
+
+    class BlurButton extends JButton implements ActionListener {
+        BlurButton(String name) {
+            super(name);             // calls the super class (JButton) constructor
+            addActionListener(this);       // adds this object (itself) as its own action listener
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            int[][] pixels = new int[myImage.getWidth()][myImage.getHeight()];
+            for (int x = 0; x < myImage.getWidth(); x++) {
+                for (int y = 0; y < myImage.getHeight(); y++) {
+                    pixels[x][y] = blurPoint(x, y);
+                }
+            }
+
+            for (int x = 0; x < myImage.getWidth(); x++) {
+                for (int y = 0; y < myImage.getHeight(); y++) {
+                    myImage.setRGB(x, y, pixels[x][y]);
+                }
+            }
+            myGraphicsPanel.repaint();
+        }
+
+        public int blurPoint(int centerX, int centerY) {
+            int l = 3;
+
+            int totalR = 0;
+            int totalG = 0;
+            int totalB = 0;
+
+            int count = 0;
+
+            for (int x = centerX - l; x <= centerX + l; x++) {
+                for (int y = centerY - l; y <= centerY + l; y++) {
+
+                    if (x >= 0 && x < myImage.getWidth() &&
+                            y >= 0 && y < myImage.getHeight()) {
+
+                        Color pixelColor = new Color(myImage.getRGB(x, y));
+
+                        totalR += pixelColor.getRed();
+                        totalG += pixelColor.getGreen();
+                        totalB += pixelColor.getBlue();
+
+                        count++;
+                    }
+                }
+            }
+
+            int avgR = totalR / count;
+            int avgG = totalG / count;
+            int avgB = totalB / count;
+
+            Color avgColor = new Color(avgR, avgG, avgB);
+
+            return avgColor.getRGB();
         }
     }
 }
